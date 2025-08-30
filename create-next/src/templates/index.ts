1675b5149479b97590a577e25301d63641212e9a
ShionTerunaga <sion2769@gmail.com>
2025-08-30 10:19:37 +0900

    save

--- create-next/src/templates/index.ts
+++ create-next/src/templates/index.ts
@@ -6,6 +6,24 @@ import fs from "fs/promises";
 import { Sema } from "async-sema";
 import { async as glob } from "fast-glob";
 
+interface DevDependencies {
+  devDependencies?: Record<string, string | undefined>;
+}
+
+interface Dependencies {
+  dependencies: Record<string, string | undefined>;
+}
+
+type PackageDict = DevDependencies &
+  Dependencies &
+  Record<
+    string,
+    string | boolean | Record<string, string | undefined> | undefined
+  >;
+
+const nextjsReactPeerVersion = "19.1.0";
+export const SRC_DIR_NAMES = ["app", "pages", "styles"];
+
 export const installTemplate = async ({
   appName,
   root,
@@ -49,15 +67,12 @@ export const installTemplate = async ({
       .replace(`"@/*": ["./src/*"]`, `"${importAlias}:`),
   );
 
+  //エイリアスの設定がデフォルトではない
   if (importAlias !== "@/*") {
     const files = await glob("**/*", {
       cwd: root,
       dot: true,
       stats: false,
-      // We don't want to modify compiler options in [ts/js]config.json
-      // and none of the files in the .git folder
-      // TODO: Refactor this to be an allowlist, rather than a denylist,
-      // to avoid corrupting files that weren't intended to be replaced
 
       ignore: [
         "tsconfig.json",
@@ -83,5 +98,65 @@ export const installTemplate = async ({
         writeSema.release();
       }),
     );
+  }
+
+  if (srcDir) {
+    await fs.mkdir(path.join(root, "src"), { recursive: true });
+    await Promise.all(
+      SRC_DIR_NAMES.map(async (file) => {
+        await fs
+          .rename(path.join(root, file), path.join(root, "src", file))
+          .catch((err) => {
+            if (err.code !== "ENOENT") throw err;
+          });
+      }),
+    );
+  }
+
+  //package.jsonの作成
+  const packageJson: PackageDict = {
+    name: appName,
+    version: "0.1.0",
+    private: true,
+    scripts: {
+      dev: "next dev",
+      build: "next build",
+      start: "next start",
+      lint: eslint ? "next lint" : undefined,
+    },
+    dependencies: {
+      react: nextjsReactPeerVersion,
+      "react-dom": nextjsReactPeerVersion,
+      next: "15.5.2",
+    },
+    devDependencies: {},
+  };
+
+  if (mode === "ts") {
+    packageJson.devDependencies = {
+      ...packageJson.devDependencies,
+      typescript: "^5",
+      "@types/node": "^20",
+      "@types/react": "^19",
+      "@types/react-dom": "^19",
+    };
+  }
+
+  if (eslint) {
+    packageJson.devDependencies = {
+      ...packageJson.devDependencies,
+      eslint: "^9",
+      "eslint-config-next": "15.5.2",
+      // TODO: Remove @eslint/eslintrc once eslint-config-next is pure Flat config
+      "@eslint/eslintrc": "^3",
+    };
+  }
+
+  if (
+    packageJson.devDependencies &&
+    typeof packageJson.devDependencies === "object"
+  ) {
+    const devDeps = Object.keys(packageJson.devDependencies).length;
+    if (!devDeps) delete packageJson.devDependencies;
   }
 };
